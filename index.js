7ce3ac453c0d479b4925af2b376b0889bb980468
Tom Buchok <tom@buchok.com>
2013-07-27 13:24:07 -0400

    adds on-off switch with a touch button

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var five = require("johnny-five")
-  , boarder = new five.Board()
-  , sensor = new five.Sensor("I0")
-  , blink = new five.Led("O0")
+  , board = new five.Board()
 ;
 
 
 board.on("ready", function() {
 
+  var sensor = new five.Sensor("I0")
+    , blink = new five.Led("O0")
+    , touch = new five.Button('O1')
+  ;
+
+  blink.state = true;
+
   sensor.scale(0, 255).on("read", function() {   		   
-      var val = Math.round(this.value);
-      blink.brightness(val);
+    var val = Math.round(this.value);
+    if (blink.state === true) blink.brightness(val);
   });
-  
+
+  touch.on('up', function() {
+    (blink.state === true) ? blink.off() : blink.on(); 
+    blink.state = !blink.state;
+  });
+
 });
