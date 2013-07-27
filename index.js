501095f61ea6fabe369e4d0e9f8ae55f9ab9d21f
Tom Buchok <tom@buchok.com>
2013-07-27 13:39:49 -0400

    adds tilt sensors

--- index.js
+++ index.js
@@ -5,21 +5,37 @@ var five = require("johnny-five")
 
 board.on("ready", function() {
 
-  var sensor = new five.Sensor("I0")
-    , blink = new five.Led("O0")
+  var slide = new five.Sensor("I0")
+    , tilt = new five.Sensor('I1')
+    , bigLight = new five.Led("O0")
     , touch = new five.Button('O1')
+    , greenLight = new five.Led('O2')
+    , redLight = new five.Led('O3')
   ;
 
-  blink.state = true;
+  bigLight.state = true;
 
-  sensor.scale(0, 255).on("read", function() {   		   
+  greenLight.off();
+  redLight.off();
+
+  tilt.on('change', function() {
+    if (this.boolean === true) {
+      greenLight.on();
+      redLight.off();  
+    } else {
+      greenLight.off();
+      redLight.on();  
+    }
+  });
+
+  slide.scale(0, 255).on("read", function() {   		   
     var val = Math.round(this.value);
-    if (blink.state === true) blink.brightness(val);
+    if (bigLight.state === true) bigLight.brightness(val);
   });
 
   touch.on('up', function() {
-    (blink.state === true) ? blink.off() : blink.on(); 
-    blink.state = !blink.state;
+    (bigLight.state === true) ? bigLight.off() : bigLight.on(); 
+    bigLight.state = !bigLight.state;
   });
 
 });
