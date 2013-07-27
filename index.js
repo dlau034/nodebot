990d80ddb6ab7092f8d269d465f0d7468a8bc7a9
Tom Buchok <tom@buchok.com>
2013-07-27 13:07:24 -0400

    cleans off comments

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-// var five = require("johnny-five"),
-// 	board = new five.Board();
-
-// board.on("ready", function(){
-
-// 	var LedR = new five.Led(9)
-// 	var LedG = new five.Led(10)
-// 	var LedB = new five.Led(11)
-	
-// 	var leds = new five.Leds();
-
-// 	this.repl.inject({
-// 		leds: leds
-// 	})
-
-// });	
+var five = require("johnny-five")
+  , boarder = new five.Board()
+  , sensor = new five.Sensor("I0")
+  , blink = new five.Led("O0")
+;
 
 
+board.on("ready", function() {
 
-
-var five = require("johnny-five");
-
-new five.Board().on("ready", function() {
-
-var sensor = new five.Sensor("I0")
-
-console.log(sensor);
-
-var blink = new five.Led("O0");
-
-  sensor.scale(0, 255).on("read", function() {
-	   		   
-      var val = Math.round(this.value)
-      console.log(val);
-
+  sensor.scale(0, 255).on("read", function() {   		   
+      var val = Math.round(this.value);
       blink.brightness(val);
-      
   });
+  
 });
-
-
-// var five = require("johnny-five");
-
-// new five.Board().on("ready", function() {
-//   new five.Led("O0").strobe(250);
-// });
-
